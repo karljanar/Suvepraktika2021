b4187ed9fa0a5adf908b1eadd6695b056fad35af
Karl Janar Kinkar <25798556+karljanar@users.noreply.github.com>
2021-06-17 20:59:32 +0300

    bug fixes

--- resources/js/Pages/Application.vue
+++ resources/js/Pages/Application.vue
@@ -88,7 +88,7 @@
 
                                 <!-- This element is to trick the browser into centering the modal contents. -->
 
-                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
+                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
 
                                 <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
 
@@ -96,7 +96,8 @@
                                         <form>
                                             <div class="">
                                                 <div class="mb-4">
-                                                    <p style="white-space: pre-wrap;">{{ commentSplit }}</p>
+                                                    <p v-if="form.comments" style="white-space: pre-wrap;">{{ commentSplit }}</p>
+                                                    <p v-else>Tühjus</p>
                                                 </div>
                                                 <button @click="closeCommentsModal()" type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                                                     Sulge
@@ -116,7 +117,7 @@
                                     <div @click="closedFrameworkModal()" class="absolute inset-0 bg-gray-500 opacity-75"></div>
                                 </div>
                                 <!-- This element is to trick the browser into centering the modal contents. -->
-                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
+                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
                                 <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
 
                                     <form>
@@ -191,7 +192,7 @@
 
                                 <!-- This element is to trick the browser into centering the modal contents. -->
 
-                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
+                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
 
                                 <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-3xl sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
 
@@ -499,8 +500,6 @@ export default {
         closeCommentsModal: function () {
 
             this.isCommentsModal = false;
-            
-            
 
         },
 
